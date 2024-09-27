fd5fac5593d39e94ba702a891fd6516f388fde93
vishnu KP <vishnuto16@gmail.com>
2024-09-27 23:41:57 +0530

    fix:remove lines from db

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -5,9 +5,7 @@ dotenv.config();
 
 const db = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useUnifiedTopology: true, 
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
